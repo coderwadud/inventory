72aaaf680e62dfb83c4df7484a1bac1743124cb5
coderwadud <user@users-MacBook-Pro.local>
2025-01-17 23:44:30 +0600

    form

---
+++ src/pages/inventory-database/create-inventory/index.tsx
@@ -0,0 +1,12 @@
+import ChsHead from "@/components/layouts/ChsHead";
+
+const CreateInventory = () => {
+	return (
+		<>
+            <ChsHead />
+            <h1>Create Inventory</h1>
+		</>
+	);
+};
+
+export default CreateInventory;
