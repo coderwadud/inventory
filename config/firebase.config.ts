6923d698b32fc4ff2f43eb9e1ef995b019198156
coderwadud <user@users-MacBook-Pro.local>
2025-01-19 21:48:27 +0600

    env file change

--- config/firebase.config.ts
+++ config/firebase.config.ts
@@ -5,12 +5,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyBKe1gXMPtujzHOIliHA6l4ED1HhvHhRiM",
-  authDomain: "fir-test-ac5c1.firebaseapp.com",
-  projectId: "fir-test-ac5c1",
-  storageBucket: "fir-test-ac5c1.firebasestorage.app",
-  messagingSenderId: "117133927399",
-  appId: "1:117133927399:web:62ae7a0d12a365786e1342"
+  // apiKey: "AIzaSyBKe1gXMPtujzHOIliHA6l4ED1HhvHhRiM",
+  // authDomain: "fir-test-ac5c1.firebaseapp.com",
+  // projectId: "fir-test-ac5c1",
+  // storageBucket: "fir-test-ac5c1.firebasestorage.app",
+  // messagingSenderId: "117133927399",
+  // appId: "1:117133927399:web:62ae7a0d12a365786e1342"
+   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase app (singleton pattern)
